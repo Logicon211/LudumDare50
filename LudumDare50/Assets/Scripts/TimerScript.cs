da2fb929b41400c7ccb4ba46522a9c4bd85888c6
Logan Marks <logicon211@gmail.com>
2022-04-02 15:22:22 -0300

    commit wip

---
+++ LudumDare50/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerScript : MonoBehaviour
+{
+    public float TimeLeft;
+    public bool TimerOn = false;
+
+    public TMP_Text timerTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimerOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TimerOn) {
+            if(TimeLeft > 0) {
+                TimeLeft -= Time.deltaTime;
+                updateTimer(TimeLeft);
+            }
+            else {
+                Debug.Log("TIMES UP");
+                TimerOn = false;
+                // TODO: Something when time's up
+            }
+        }
+    }
+
+    void updateTimer(float currentTime) {
+        currentTime+=1;
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timerTxt.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+}
