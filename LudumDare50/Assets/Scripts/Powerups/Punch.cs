6ed91b3096ab8acc29c72cb833cd9cb8fb8f5435
Chad Gerein <chadifferus@gmail.com>
2022-04-02 19:49:27 -0300

    adding rotation and making punch flip sides depending on which way you're facing

--- LudumDare50/Assets/Scripts/Powerups/Punch.cs
+++ LudumDare50/Assets/Scripts/Powerups/Punch.cs
@@ -10,11 +10,18 @@ public class Punch : Powerup
   public GameObject punchProjectile;
 
   public override void UsePowerup() {
+
     SpawnProjectile();
   }
 
   public void SpawnProjectile() {
-    GameObject projectile = Instantiate(punchProjectile, rightPunch.transform);
+    Vector3 spawn = new Vector3(playerObject.transform.position.x + 1.5f, playerObject.transform.position.y , 0);
+    GameObject projectile = Instantiate(punchProjectile, player.transform);
+    if (player.GetDirection() == "left") {
+     spawn.x = spawn.x - 3f;
+     projectile.transform.Rotate(new Vector3(0,180,0));
+     } 
+    projectile.transform.position = spawn;
     projectile.transform.parent = gameArea.transform;
   }
 }
