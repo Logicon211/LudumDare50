612f42a27263414d07a1a9f7a7831ab42e2a09a3
Chad Gerein <chadifferus@gmail.com>
2022-04-02 01:45:00 -0300

    adding more stuff to powerup abstract class

--- LudumDare50/Assets/Scripts/Powerups/Powerup.cs
+++ LudumDare50/Assets/Scripts/Powerups/Powerup.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using Enemy.Interface;
+using System;
 
 public abstract class Powerup : MonoBehaviour {
+
+    [Serializable]
+    protected struct stats {
+        [SerializeField] public float damage;
+        [SerializeField] public float speed;
+        [SerializeField] public float cooldown;
+        [SerializeField] public int projectiles;
+    }
     [SerializeField] protected string[] levelDescriptions;
-    [SerializeField] protected int level;
+    [SerializeField] protected stats[] levelStats;
+    private int Level;
+    [SerializeField] protected int level {
+        get {
+            return Level;
+        }
+        set {
+            Level = value;
+            SetStats(Level);
+        }
+    }
     [SerializeField] protected float damage;
     [SerializeField] protected float speed;
     [SerializeField] protected float cooldown;
+    [SerializeField] protected int projectiles;
+    [SerializeField] protected stats currentStats;
 
     float currentCooldown = 0f;
 
     void Start() {
-
+        level = 0;
     }
 
     void FixedUpdate() {
@@ -34,6 +56,18 @@ public abstract class Powerup : MonoBehaviour {
         level += numberOfLevels;
     }
 
+    public void SetStats(int newLevel) {
+        if (levelStats.Length < newLevel) {
+            Debug.Log("No stats for that level");
+            return;
+        }
+        Debug.Log("yrdyrdy");
+        currentStats.damage = levelStats[newLevel].damage;
+        currentStats.speed = levelStats[newLevel].speed;
+        currentStats.cooldown = levelStats[newLevel].cooldown;
+        currentStats.projectiles = levelStats[newLevel].projectiles;
+    }
+
     public string GetPowerupLevelDescription(int level) {
         if (levelDescriptions.Length == 0) {
             return "No description";
@@ -51,5 +85,15 @@ public abstract class Powerup : MonoBehaviour {
             return true;
         }
         return false;
+    }
+
+    // Probably will work, just a generic entry point that we might be able to hit to calculate damage
+    protected void DoDamage(GameObject target) {
+        IDamageable<float> targetDamage = target.GetComponent<IDamageable<float>>();
+
+        // Get the players total bonus
+
+        // Add total bonus to damage
+        targetDamage.Damage(damage);
     }
 }
--- LudumDare50/Assets/Scripts/Powerups/Punch.cs
+++ LudumDare50/Assets/Scripts/Powerups/Punch.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Punch : Powerup
 {
   public override void UsePowerup() {
+    LevelUp(1);
     Debug.Log(GetPowerupLevelDescription(1));
   }
 
