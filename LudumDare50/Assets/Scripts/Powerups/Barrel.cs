609b7239698dbfb86dc9a5c96f6a601276f23ae4
Chad Gerein <chadifferus@gmail.com>
2022-04-03 20:57:26 -0300

    adding barrel size change on level up

--- LudumDare50/Assets/Scripts/Powerups/Barrel.cs
+++ LudumDare50/Assets/Scripts/Powerups/Barrel.cs
@@ -7,18 +7,38 @@ public class Barrel : Powerup {
     public GameObject barrelProjectile;
     // Dont want the bomb spawning near the player
     public float noSpawnArea;
-    public float explosionRadius;
+
+    public float explosionRadius = 1.5f;
+
+    // This is the one that gets set 
+    public float actualExplosionRadius;
+
+    protected override void Start()
+    {
+        base.Start();
+        actualExplosionRadius = explosionRadius;
+    }
 
     public override void UsePowerup() {
         StartCoroutine(StartPowerup());
     }
 
     private IEnumerator StartPowerup() {
+        Debug.Log("about to spawn");
         for (int i = 0; i < currentStats.projectiles + player.playerStats.overdrive; i++) {
             SpawnProjectile();
             yield return new WaitForSeconds(currentStats.projectileSpeed);
         }
         yield return null;
+    }
+
+    public override void SetStats(int newLevel) {
+        base.SetStats(newLevel);
+        if (newLevel < 3) {
+            actualExplosionRadius = explosionRadius;
+        } else {
+            actualExplosionRadius = explosionRadius * 2;
+        }
     }
 
     // https://answers.unity.com/questions/1580130/i-need-to-instantiate-an-object-inside-a-donut-ins.html
@@ -28,6 +48,7 @@ public class Barrel : Powerup {
         return Random.insideUnitCircle.normalized * radius;
     }
     private void SpawnProjectile() {
+        Debug.Log("spawning");
         Vector2 randomPoint = GetRandomPointInsideDonut(noSpawnArea, spawnRadius) + transform.position;
 
         GameObject barrel = Instantiate(barrelProjectile, gameArea.transform, true);
